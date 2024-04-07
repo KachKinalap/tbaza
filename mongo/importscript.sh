96e02eeefb6a1c612cdb958fa3e22edc1852e8ca
KachKinalap <KachKinalap@yandex.ru>
2024-04-07 11:48:18 +0300

    mongo fix 3

--- mongo/importscript.sh
+++ mongo/importscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mongoimport --db='whitepeak_db' --collection='main' --file='/tmp/json/main.json' --username='root' --password='HrigHdofkroJndi' --authenticationDatabase=admin
-mongoimport --db='whitepeak_db' --collection='price' --file='/tmp/json/price/price.json' --jsonArray --username='HrigHdofkroJndi' --password='root' --authenticationDatabase=admin
-mongoimport --db='whitepeak_db' --collection='price' --file='/tmp/json/price/price_preview.json' --username='HrigHdofkroJndi' --password='root' --authenticationDatabase=admin
+mongoimport --db='whitepeak_db' --collection='price' --file='/tmp/json/price/price.json' --jsonArray --username='root' --password='HrigHdofkroJndi' --authenticationDatabase=admin
+mongoimport --db='whitepeak_db' --collection='price' --file='/tmp/json/price/price_preview.json' --username='root' --password='HrigHdofkroJndi' --authenticationDatabase=admin
 mongoimport --db='whitepeak_db' --collection='about' --file='/tmp/json/about.json' --username='root' --password='HrigHdofkroJndi' --authenticationDatabase=admin
 mongoimport --db='whitepeak_db' --collection='events' --file='/tmp/json/events.json' --username='root' --password='HrigHdofkroJndi' --authenticationDatabase=admin
