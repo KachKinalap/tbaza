23d6f24fd8629a455cba1fb8001c53e2deceb9c9
KachKinalap <KachKinalap@yandex.ru>
2024-05-19 21:54:36 +0300

    fixing app title

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import AppRouter from "./components/AppRouter";
+import React, { useEffect } from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { YMaps } from '@pbe/react-yandex-maps';
 
 function App() {
+
+    useEffect(()=>{
+        document.title = 'Белая Гора';
+    },[])
+
   return (
     <div className="App">
         <YMaps>
